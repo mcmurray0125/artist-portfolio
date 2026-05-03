3046bdf14e86883a77104587d988d268d025485c
Michael Murray <mcmurray0125@gmail.com>
2026-05-02 22:29:25 -0500

    center the carousel on home page

--- apps/web/src/components/HomePageCarousel.jsx
+++ apps/web/src/components/HomePageCarousel.jsx
@@ -12,7 +12,6 @@ import CarouselTile from './CarouselTile'
 import QuickViewModal from './QuickViewModal'
 
 export const HomePageCarousel = ({ featuredArtworks }) => {
-  console.log(featuredArtworks);
   const [selectedArtwork, setSelectedArtwork] = useState(null)
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
@@ -32,7 +31,7 @@ export const HomePageCarousel = ({ featuredArtworks }) => {
   const options = {
     loop: true,
     skipSnaps: true,
-    inViewMargin: '0px -16px 0px 0px'
+    inViewMargin: '0px 48px 0px 0px'
   };
 
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [
@@ -50,40 +49,42 @@ export const HomePageCarousel = ({ featuredArtworks }) => {
   } = usePrevNextButtons(emblaApi)
 
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {featuredArtworks?.map((artwork, index) => (
-            <CarouselTile
-              key={index}
-              images={artwork.images}
-              title={artwork.title}
-              description={artwork.description}
-              slug={artwork.slug}
-              onClick={() => handleTileClick(artwork)}
-            />
-          ))}
+    <div className='carousel-wrapper'>
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {featuredArtworks?.map((artwork, index) => (
+              <CarouselTile
+                key={index}
+                images={artwork.images}
+                title={artwork.title}
+                description={artwork.description}
+                slug={artwork.slug}
+                onClick={() => handleTileClick(artwork)}
+              />
+            ))}
+          </div>
         </div>
+        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        {isModalOpen && selectedArtwork && (
+          <QuickViewModal
+            artwork={selectedArtwork}
+            currentImageIndex={currentImageIndex}
+            onNextImage={() => {
+              if (currentImageIndex < selectedArtwork.images.length - 1) {
+                setCurrentImageIndex(currentImageIndex + 1)
+              }
+            }}
+            onPrevImage={() => {
+              if (currentImageIndex > 0) {
+                setCurrentImageIndex(currentImageIndex - 1)
+              }
+            }}
+            onClose={handleCloseModal}
+          />
+        )}
       </div>
-      <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-      <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-      {isModalOpen && selectedArtwork && (
-        <QuickViewModal
-          artwork={selectedArtwork}
-          currentImageIndex={currentImageIndex}
-          onNextImage={() => {
-            if (currentImageIndex < selectedArtwork.images.length - 1) {
-              setCurrentImageIndex(currentImageIndex + 1)
-            }
-          }}
-          onPrevImage={() => {
-            if (currentImageIndex > 0) {
-              setCurrentImageIndex(currentImageIndex - 1)
-            }
-          }}
-          onClose={handleCloseModal}
-        />
-      )}
     </div>
   )
 }
