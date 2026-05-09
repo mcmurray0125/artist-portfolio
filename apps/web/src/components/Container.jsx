75400fe891015c1e0daf8e04cff9dff04c6e9191
Michael Murray <mcmurray0125@gmail.com>
2026-05-09 00:02:34 -0500

    container bottom padding

--- apps/web/src/components/Container.jsx
+++ apps/web/src/components/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className="mx-auto min-h-[calc(100vh-var(--spacing-header-total))] pt-header-total max-w-7xl px-6">
+    <div className="mx-auto min-h-[calc(100vh-var(--spacing-header-total))] pt-header-total max-w-7xl px-6 pb-10">
       {children}
     </div>
   )
