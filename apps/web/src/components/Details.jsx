8266e64b7302e2d63f4f4453e51ae0950f9919cf
Michael Murray <mcmurray0125@gmail.com>
2026-05-08 23:42:34 -0500

    conditional rendering of artwork details

--- apps/web/src/components/Details.jsx
+++ apps/web/src/components/Details.jsx
@@ -3,11 +3,10 @@ import { PortableText } from '@portabletext/react'
 export default function Details({ artwork }) {
     return (
         <>
-            <h1 className="text-4xl text-gray-200 font-bold mb-4">{artwork.title}</h1>
-            <p>Medium: {artwork.medium}</p>
-            <p>Dimensions: {artwork.dimensions}</p>
-            <p>Year: {artwork.year}</p>
-            <p>Price: ${artwork.price?.toFixed(2)}</p>
+            {artwork.year && <p>Year: {artwork.year}</p>}
+            {artwork.medium && <p>Medium: {artwork.medium}</p>}
+            {artwork.dimensions && <p>Dimensions: {artwork.dimensions}</p>}
+            {artwork.price !== undefined && <p>Price: ${artwork.price?.toFixed(2)}</p>}
             <PortableText value={artwork.description} />
         </>
     )
