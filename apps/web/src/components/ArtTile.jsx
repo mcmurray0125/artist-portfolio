eed64ce231a5426bd56a25fed8f4e049e4d99ecc
Michael Murray <mcmurray0125@gmail.com>
2026-05-09 18:04:17 -0500

    remove blue text Art Tile links

--- apps/web/src/components/ArtTile.jsx
+++ apps/web/src/components/ArtTile.jsx
@@ -5,12 +5,12 @@ import { urlFor } from '../sanity/utilities';
 const ArtTile = ({ images, title, description, slug }) => {
   return (
     <div className="card shadow-md overflow-hidden">
-      <Link to={`/artwork-detail/${slug.current}`} className="text-blue-500 hover:underline">
+      <Link to={`/artwork-detail/${slug.current}`} className="hover:underline">
         <img src={urlFor(images[0]).width(800).url()} alt={title} className="w-full h-auto object-cover" />
       </Link>
       <div className="p-4">
         <h2 className="text-xl font-semibold mb-2">{title}</h2>
-        <Link to={`/artwork-detail/${slug.current}`} className="text-blue-500 hover:underline">
+        <Link to={`/artwork-detail/${slug.current}`} className="hover:underline">
           View Details
         </Link>
       </div>
